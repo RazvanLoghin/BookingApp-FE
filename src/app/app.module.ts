ffecd5eeb153a93bbf2242d7ca7b501ed7cf9cb7
razvanloghin <rloghin55@yahoo.com>
2021-02-27 14:17:51 +0200

    Last version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import {MatFormFieldModule } from '@angular/material/form-field';
-import {MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import {MatIcon, MatIconModule} from '@angular/material/icon';
-
-import { ReactiveFormsModule } from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatCardModule,
-    MatButtonModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    RouterModule,
-    MatIconModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
